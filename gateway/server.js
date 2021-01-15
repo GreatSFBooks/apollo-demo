0c8bb60d566ce6d30bc533eb04a2d06067b9f112
Jesse Lovelace <jesse.lovelace@gmail.com>
2021-01-15 12:17:42 -0500

    Update cron expression.

--- gateway/server.js
+++ gateway/server.js
@@ -33,11 +33,9 @@ const gateway = new ApolloGateway({
 
 const server = new ApolloServer({
   gateway,
-  playground: true,
   debug:true,
   subscriptions: false,
-  logger: logger,
-  introspection: true,
+  logger: logger
 });
 
 server.listen({ port: PORT }).then(({ url }) => {
